efc34919b235bba07876462acaecdaba5fb09219
Eetin <p17dmitry@gmail.com>
2013-07-06 16:46:28 +0000

    Edited web.js to really display index.html (2)

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    Buffer indexBuf = fs.readFileSync("index.html");
+    var indexBuf = fs.readFileSync("index.html");
     response.send(indexBuf.toString());
 });
 
